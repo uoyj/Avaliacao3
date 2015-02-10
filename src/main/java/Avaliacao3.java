6ffe470f8a1b03439ca6f3efdd4524aff053efb8
Jhonatan <Jhonatan@192.168.108.41>
2015-02-10 11:55:59 -0200

    First commit

--- src/main/java/Avaliacao3.java
+++ src/main/java/Avaliacao3.java
@@ -1,9 +1,14 @@
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import utfpr.ct.dainf.if62c.avaliacao.Lancamento;
+import utfpr.ct.dainf.if62c.avaliacao.ProcessaLancamentos;
 
 /**
  * IF62C Fundamentos de Programação 2
@@ -12,17 +17,43 @@ import utfpr.ct.dainf.if62c.avaliacao.Lancamento;
  */
 public class Avaliacao3 {
  
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+            Scanner sc = new Scanner(System.in);
+            
+            System.out.println("Digite o caminho: ");
+            String path = sc.nextLine();
+            ProcessaLancamentos PL;
+            Integer conta = 1;
+            
+            if(new File(path).exists()) PL = new ProcessaLancamentos(new File(path));
+            else throw new FileNotFoundException();
+            
+            List<Lancamento> lista = PL.getLancamentos();
+            
+            while(!conta.equals(0))
+            {
+                while(!sc.hasNextInt()) System.out.println("Por favor, informe um valor numérico");                    
+                conta = sc.nextInt();
+                exibeLancamentosConta(lista, conta);
+            }
+            
     }
     
     public static void exibeLancamentosConta(List<Lancamento> lancamentos, Integer conta) {            
+            //List<Lancamento> lista = new LinkedList<>();
+            boolean achou = false;
             for(Lancamento l : lancamentos)
             {
                 if(l.getConta().equals(conta))
-                    System.out.println(l.toString());
+                {
+                    achou = true;
+                    System.out.println(l);
+                    //lista.add(l);
+                }
+                if(!achou) System.out.println("Conta inexistente");
             }
-
+            //if(lista.isEmpty()) return null;
+            //else return lista;
     }
  
 }
